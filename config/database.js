996e348d58e4ba6d09aa67e53b3cf24f3379a47d
omr <omradelbakry375@gmail.com>
2025-04-10 03:28:17 +0200

    modify db configurations

--- config/database.js
+++ config/database.js
@@ -4,10 +4,6 @@ const dbConnection = () => {
   mongoose.connect(process.env.DB_URL).then((connect) => {
     console.log(`DB connected ${connect.connection.host}`);
   });
-  // .catch((error) => {
-  //   console.log(`DB faild to connect ${error}`);
-  //   process.exit(1);
-  // });
 };
 
 export default dbConnection;
